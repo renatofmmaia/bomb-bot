5b161052d3b0cfb322439455e61de8ac50d3594a
Hiago Inacio <hiago.inacio@mag-it.com.br>
2022-01-19 14:44:22 -0300

    fix print_screen  and get_one_target_position

--- module/image.py
+++ module/image.py
@@ -33,20 +33,20 @@ class Image:
     @staticmethod
     def get_target_positions(target:str):
         target_img = Image.TARGETS[target]
-        screen_img = Image.print_screen()
+        screen_img = Image.screen()
         return cv2.matchTemplate(screen_img, target_img, cv2.TM_CCOEFF_NORMED)
     
     @staticmethod
     def get_one_target_position(target:str, threshold:float=0.8):
         target_img = Image.TARGETS[target]
-        screen_img = Image.print_screen()
+        screen_img = Image.screen()
         result = cv2.matchTemplate(screen_img, target_img, cv2.TM_CCOEFF_NORMED)
 
         if result.max() < threshold:
             raise Exception(f"{target} not found")
             
-        width, hight = target.shape[:2]
-        yloc, xloc = np.argmax(result)
+        width, hight = target_img.shape[:2]
+        yloc, xloc = np.where(result == result.max())
         return xloc, yloc, width, hight
 
 
