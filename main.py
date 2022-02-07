d88e6e5994e3e3f8f80dd1928c8fec7edfae51b2
Hiago Inacio <hiago.inacio@mag-it.com.br>
2022-02-06 21:13:53 -0300

    update version

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from module.logger import logger, reset_log_file
 from module.manager import create_bombcrypto_managers
 from module.telegram import TelegramBot
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 
 def main(config_file):
